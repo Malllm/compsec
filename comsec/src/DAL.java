73ff727336186629eb27138709220b09d57cba74
dat11gma <dat11gma@alfa-5.student.lth.se>
2014-02-18 16:04:17 +0100

    Person/ db methods

--- comsec/src/DAL.java
+++ comsec/src/DAL.java
@@ -1,7 +1,31 @@
 
-public class DAL {
+import java.util.ArrayList;
 
+
+public class DAL {
+	public boolean removeFile(int Pnr){
+		return false;
+	}
+    public boolean addFile(int Pnr){
+    	return false;
+    }
+    public boolean createPerson(int Pnr, ArrayList<Person> list){
+    	return false;
+    }
+    public boolean editJournal(int Pnr, String txt){
+    	return false;
+    }
+    public String readJournal(int Pnr){
+    	return "";
+    }
+    public boolean checkPermission(int ID, int pnr){
+    	return false;
+    }
+    public boolean remove(int pnr){    //kanske ID istället
+    	return false;
+    }
 }
+
 
 /*
 	TEST TEST
