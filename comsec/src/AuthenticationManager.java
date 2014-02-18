f6d38269d9dcbc260d03dc7c812c3bd7200f0d1b
Kasper Rundquist <atf10kru@student.lu.se>
2014-02-18 19:31:24 +0100

    Added getJournal, addJournal and editJournal

--- comsec/src/AuthenticationManager.java
+++ comsec/src/AuthenticationManager.java
@@ -1,5 +1,6 @@
 import java.sql.*;
 
+
 public class AuthenticationManager {
 
 	private Connection conn;
@@ -15,4 +16,73 @@ public class AuthenticationManager {
 			e.printStackTrace();
 		}
 	}
+	
+	public void addJournal(int pnr, int doctorID, int nurseID, String journal) { //addPerson i Gdocs
+		String sql = "INSERT INTO Journals VALUES(?, ?, ?, ?)";
+		PreparedStatement ps = null;
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, pnr);
+			ps.setInt(2, doctorID);
+			ps.setInt(3, nurseID);
+			ps.setString(4, journal);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				ps.close();
+				}  catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
+	}
+	
+	public void addJournal(String txt, int pnr) {
+		String sql = "UPDATE Journals SET journal = CONCAT(journal, ?) WHERE pnr = ?";
+		PreparedStatement ps = null;
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, txt);
+			ps.setInt(2, pnr);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				ps.close();
+				}  catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
+	}
+	
+	public String getJournal(int pnr) {
+		String sql = "SELECT journal FROM Journals WHERE pnr = ?";
+		PreparedStatement ps = null;
+		ResultSet rs;
+		String txt = null;
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, pnr);
+			rs = ps.executeQuery();
+			txt = rs.getString("journal");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				ps.close();
+				}  catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
+		return txt;
+		
+	}
 }
+
+
+
+
+
+
