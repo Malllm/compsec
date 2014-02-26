9ec4e895496f92b5d224f16062343f9d810691a3
ludd-e <st.ludde@gmail.com>
2014-02-26 17:29:45 +0100

    Update server.java

--- comsec/src/server.java
+++ comsec/src/server.java
@@ -36,177 +36,180 @@ public class server implements Runnable {
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
     public void run() {
-        try {
-        	//v�ntar p� att en klient ska connecta och skapar en ny tr�d f�r ytterligare klienter
-        	SSLSocket socket=(SSLSocket)serverSocket.accept();
-            newListener();
-            
-            // Skapar logfil om den ej finns.
-            File file = new File(filepath);
-            if(!file.exists()){
-            	file.createNewFile();
-            }
-            
-            // l�ser av clientens cert och h�mtar namn
-            SSLSession session = socket.getSession();          
-            X509Certificate cert = (X509Certificate)session.getPeerCertificateChain()[0];
-            String subject = cert.getSubjectDN().getName();
-           
-            KeyStore ts = KeyStore.getInstance("JKS");
-            ts.load(new FileInputStream("servertruststore"), "server".toCharArray()); // truststore password (storepass);
-            cert.verify(ts.getCertificate("CA").getPublicKey());
-            
-            
-            // skriver ut den nya klientens namn
-            System.out.println("client connected");
-            System.out.println("client name (cert subject DN field): " + subject);
-            
-            logConnection(subject);
-            
-            //skapar en ny reader och writer p� socketen
-            PrintWriter out = null;
-            BufferedReader in = null;
-            out = new PrintWriter(socket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            
-            
-            int usertype;
-            if(subject.length() > 4){
-            	usertype = PRIVATEPERSON;
-            }
-            else if(subject.substring(2).equals("01")){
-            	usertype = NURSE;
-            }
-            else if(subject.substring(2).equals("10")){
-            	System.out.println("Doctor");
-            	usertype = DOCTOR;
-            }
-            else{
-            	usertype = GOVERMENTAGENT;
-            }
-            String journal;
-            switch(usertype){
-            	case 0:	
-            		journal = am.getJournal(Integer.parseInt(subject));
-            		out.println(journal);   
-            		logRead(subject, Long.parseLong(subject));
-            		break;
-            	case 1: //Nurse
-                	while(true){
-                		out.println("Write to journal[w] or read journal[r] ");
-                		String temp = in.readLine();
-                			if(temp.startsWith("q") || temp.startsWith("Q")){
-                				break;
-                			}
-                			if(temp.equals("r")){
-                				long pnr = readPnr(in, out);
-                				journal = am.getJournal(pnr, Integer.parseInt(subject), "nurseID"); 		//Var görs rättighetskollen?
-                            	out.println(journal);														//I hämtningen från databasen
-                            	
-                            	logRead(subject, pnr);
-                			}else if(temp.equals("w")){  
-                				long pnr = readPnr(in, out);
-                				out.println("Text to add to journal: ");
-                				temp = in.readLine();
-                				am.updateJournal(temp, pnr, Integer.parseInt(subject), "nurseID");
-                				
-                				logWrite(subject, pnr);
-                			}
-                		}
-            		break;
-            	case 2: //Doctor
-            		//read/write/create?
-            		//ask for which pnr doctor wants?
-            		//return journal if access else return error
-            		while(true){
-                		out.println("Create journal[c], write to journal[w] or read journal[r] ");
-                		String temp = in.readLine();
-                			if(temp.startsWith("q") || temp.startsWith("Q")){
-                				break;
-                			}
-                			if(temp.equals("r")){
-                				long pnr = readPnr(in, out);
-                				journal = am.getJournal(pnr, Integer.parseInt(subject), "doctorID"); 
-                            	out.println(journal);
-                            	
-                            	logRead(subject, pnr);
-                			}else if(temp.equals("w")){  
-                				long pnr = readPnr(in, out);
-                				out.println("Text to add to journal: ");
-                				temp = in.readLine();
-                				am.updateJournal(temp, pnr, Integer.parseInt(subject), "doctorID");
-                				
-                				logWrite(subject, pnr);
-                			}else if(temp.equals("c")) {
-                				long pnr = readPnr(in, out);
-                				int nurseID = readNurseID(in, out);
-                				out.println("Text to insert into journal: ");
-                				temp = in.readLine();
-                				am.createJournal(pnr, Integer.parseInt(subject), nurseID, temp);
-                				
-                				logCreate(subject, nurseID, pnr);
-                			}
-                		}
-            		break;
-            	case 3: //Govermentagent
-            		//read/delete?
-            		//ask for which pnr agent wants?
-            		//return or delete journal
-            		while(true){
-                		out.println("Read journal[r] or delete journal[d]");
-                		String temp = in.readLine();
-                			if(temp.startsWith("q") || temp.startsWith("Q")){
-                				break;
-                			}
-                			if(temp.equals("r")){
-                				long pnr = readPnr(in, out);
-                				journal = am.getJournal(pnr); 
-                            	out.println(journal);
-                            	
-                            	logRead(subject, pnr);
-                			}else if(temp.equals("d")){  
-                				long pnr = readPnr(in, out);
-                				am.deleteJournal(pnr);
-                				
-                				logDelete(subject, pnr);
-                			}
-                		}
-            		break;
-            }
-            
-         
-			in.close();
-			out.close();
-			socket.close();
-            System.out.println("client disconnected");
-            logExit(subject);
-		} catch (IOException e) {
-            System.out.println("Client died: " + e.getMessage());
-            e.printStackTrace();
-            return;
-        } catch (KeyStoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (CertificateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchProviderException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SignatureException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (javax.security.cert.CertificateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
+    	while(true){
+    		try {
+            	//v�ntar p� att en klient ska connecta och skapar en ny tr�d f�r ytterligare klienter
+            	SSLSocket socket=(SSLSocket)serverSocket.accept();
+                newListener();
+                
+                // Skapar logfil om den ej finns.
+                File file = new File(filepath);
+                if(!file.exists()){
+                	file.createNewFile();
+                }
+                
+                // l�ser av clientens cert och h�mtar namn
+                SSLSession session = socket.getSession();          
+                X509Certificate cert = (X509Certificate)session.getPeerCertificateChain()[0];
+                String subject = cert.getSubjectDN().getName();
+               
+                KeyStore ts = KeyStore.getInstance("JKS");
+                ts.load(new FileInputStream("servertruststore"), "server".toCharArray()); // truststore password (storepass);
+                cert.verify(ts.getCertificate("CAserver").getPublicKey());
+                
+                
+                // skriver ut den nya klientens namn
+                System.out.println("client connected");
+                System.out.println("client name (cert subject DN field): " + subject);
+                
+                logConnection(subject);
+                
+                //skapar en ny reader och writer p� socketen
+                PrintWriter out = null;
+                BufferedReader in = null;
+                out = new PrintWriter(socket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                
+                
+                int usertype;
+                if(subject.length() > 4){
+                	usertype = PRIVATEPERSON;
+                }
+                else if(subject.substring(2).equals("01")){
+                	usertype = NURSE;
+                }
+                else if(subject.substring(2).equals("10")){
+                	System.out.println("Doctor");
+                	usertype = DOCTOR;
+                }
+                else{
+                	usertype = GOVERMENTAGENT;
+                }
+                String journal;
+                switch(usertype){
+                	case 0:	
+                		journal = am.getJournal(Integer.parseInt(subject));
+                		out.println(journal);   
+                		logRead(subject, Long.parseLong(subject));
+                		break;
+                	case 1: //Nurse
+                    	while(true){
+                    		out.println("Write to journal[w] or read journal[r] ");
+                    		String temp = in.readLine();
+                    			if(temp.startsWith("q") || temp.startsWith("Q")){
+                    				break;
+                    			}
+                    			if(temp.equals("r")){
+                    				long pnr = readPnr(in, out);
+                    				journal = am.getJournal(pnr, Integer.parseInt(subject), "nurseID"); 		//Var görs rättighetskollen?
+                                	out.println(journal);														//I hämtningen från databasen
+                                	
+                                	logRead(subject, pnr);
+                    			}else if(temp.equals("w")){  
+                    				long pnr = readPnr(in, out);
+                    				out.println("Text to add to journal: ");
+                    				temp = in.readLine();
+                    				am.updateJournal(temp, pnr, Integer.parseInt(subject), "nurseID");
+                    				
+                    				logWrite(subject, pnr);
+                    			}
+                    		}
+                		break;
+                	case 2: //Doctor
+                		//read/write/create?
+                		//ask for which pnr doctor wants?
+                		//return journal if access else return error
+                		while(true){
+                    		out.println("Create journal[c], write to journal[w] or read journal[r] ");
+                    		String temp = in.readLine();
+                    			if(temp.startsWith("q") || temp.startsWith("Q")){
+                    				break;
+                    			}
+                    			if(temp.equals("r")){
+                    				long pnr = readPnr(in, out);
+                    				journal = am.getJournal(pnr, Integer.parseInt(subject), "doctorID"); 
+                                	out.println(journal);
+                                	
+                                	logRead(subject, pnr);
+                    			}else if(temp.equals("w")){  
+                    				long pnr = readPnr(in, out);
+                    				out.println("Text to add to journal: ");
+                    				temp = in.readLine();
+                    				am.updateJournal(temp, pnr, Integer.parseInt(subject), "doctorID");
+                    				
+                    				logWrite(subject, pnr);
+                    			}else if(temp.equals("c")) {
+                    				long pnr = readPnr(in, out);
+                    				int nurseID = readNurseID(in, out);
+                    				out.println("Text to insert into journal: ");
+                    				temp = in.readLine();
+                    				am.createJournal(pnr, Integer.parseInt(subject), nurseID, temp);
+                    				
+                    				logCreate(subject, nurseID, pnr);
+                    			}
+                    		}
+                		break;
+                	case 3: //Govermentagent
+                		//read/delete?
+                		//ask for which pnr agent wants?
+                		//return or delete journal
+                		while(true){
+                    		out.println("Read journal[r] or delete journal[d]");
+                    		String temp = in.readLine();
+                    			if(temp.startsWith("q") || temp.startsWith("Q")){
+                    				break;
+                    			}
+                    			if(temp.equals("r")){
+                    				long pnr = readPnr(in, out);
+                    				journal = am.getJournal(pnr); 
+                                	out.println(journal);
+                                	
+                                	logRead(subject, pnr);
+                    			}else if(temp.equals("d")){  
+                    				long pnr = readPnr(in, out);
+                    				am.deleteJournal(pnr);
+                    				
+                    				logDelete(subject, pnr);
+                    			}
+                    		}
+                		break;
+                }
+                
+             
+    			in.close();
+    			out.close();
+    			socket.close();
+                System.out.println("client disconnected");
+                logExit(subject);
+    		} catch (IOException e) {
+                System.out.println("Client died: " + e.getMessage());
+                e.printStackTrace();
+                return;
+            } catch (KeyStoreException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		} catch (NoSuchAlgorithmException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		} catch (CertificateException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		} catch (InvalidKeyException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		} catch (NoSuchProviderException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		} catch (SignatureException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		} catch (javax.security.cert.CertificateException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+        }
+    	}
+        
     
     private long readPnr(BufferedReader in, PrintWriter out) {
     	String temp;
@@ -284,10 +287,10 @@ public class server implements Runnable {
     private void newListener() { (new Thread(this)).start(); } // calls run()
     public static void main(String args[]) {
         System.out.println("\nServer Started\n");
-        int port = -1;
-        if (args.length >= 1) {
+        int port = 8080;
+        /*if (args.length >= 1) {
             port = Integer.parseInt(args[0]);
-        }
+        }*/
         String type = "TLS";
         try {
             ServerSocketFactory ssf = getServerSocketFactory(type);
@@ -315,7 +318,7 @@ public class server implements Runnable {
                 TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
                 KeyStore ks = KeyStore.getInstance("JKS");
 				KeyStore ts = KeyStore.getInstance("JKS");
-                char[] password = "password".toCharArray();
+                char[] password = "server".toCharArray();
 
                 ks.load(new FileInputStream("serverkeystore"), password);  // keystore password (storepass)
                 ts.load(new FileInputStream("servertruststore"), password); // truststore password (storepass)
